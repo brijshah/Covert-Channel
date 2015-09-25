abdfadda5d9afdbf4e4b8e9cf739b1dd8caee4b8
Brij Shah <brijshah0@gmail.com>
2015-09-25 16:04:55 -0700

    removed unused methods in client

--- client.py
+++ client.py
@@ -13,14 +13,6 @@ parser.add_argument('-d', '--destination', dest='dest_ip', help='destination IP 
 parser.add_argument('-m', '--message', dest='msg', help='message to send', required=True)
 args = parser.parse_args()
 
-def encrypt(message, key):
-	des = ARC4.new(key)
-	return des.encrypt(message)
-
-def decrypt(cipher_text, key):
-	des = ARC4.new(key)
-	return des.decrypt(cipher_text)
-
 def craft_packet(int):
 	global pkt
 	pkt = IP(dst=args.dest_ip, src="192.168.0."+`int`)/TCP(sport=RandNum(0, 65335), dport=8000, window=4096)
